c8bd1c01597c4d7ee189afd3a697342772b5c196
wangchao <wchao0829@163.com>
2018-01-30 13:17:22 +0800

    第一次提交

--- app/src/main/java/com/android/facereco/MainActivity.java
+++ app/src/main/java/com/android/facereco/MainActivity.java
@@ -1,13 +1,63 @@
 package com.android.facereco;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.kongqw.permissionslibrary.PermissionsManager;
 
+public class MainActivity extends BaseActivity {
+    private PermissionsManager mPermissionsManager;
+    // 要校验的权限
+    private final String[] PERMISSIONS = new String[]{Manifest.permission.CAMERA};
+    // 识别请求码
+    private final int REQUEST_CODE_DETECTION = 0;
+    // 追踪请求码
+    private final int REQUEST_CODE_TRACK = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPermissionsManager = new PermissionsManager(this) {
+            @Override
+            public void authorized(int i) {
+                switch (i){
+                    case REQUEST_CODE_DETECTION:
+                        startActivity(new Intent(MainActivity.this,ObjectDetectingActivity.class));
+                        break;
+                    case REQUEST_CODE_TRACK:
+                        startActivity(new Intent(MainActivity.this,ObjectTrackingActivity.class));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            @Override
+            public void noAuthorization(int i, String[] strings) {
+                showPermissionDialog();
+            }
+
+            @Override
+            public void ignore(int i) {
+                //Android 6.0 以下不进行权限检查
+                authorized(i);
+            }
+        };
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mPermissionsManager.recheckPermissions(requestCode,permissions,grantResults);
+    }
+
+    //目标检测
+    public  void onDetecting(View view){
+        mPermissionsManager.checkPermissions(REQUEST_CODE_DETECTION,PERMISSIONS);
+    }
+    //目标追踪
+    public  void onTracking(View view){
+        mPermissionsManager.checkPermissions(REQUEST_CODE_TRACK,PERMISSIONS);
     }
 }
---
+++ app/src/main/java/com/android/facereco/ObjectTrackingActivity.java
@@ -0,0 +1,105 @@
+package com.android.facereco;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.kongqw.ObjectTrackingView;
+import com.kongqw.listener.OnCalcBackProjectListener;
+import com.kongqw.listener.OnObjectTrackingListener;
+import com.kongqw.listener.OnOpenCVLoadListener;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+
+public class ObjectTrackingActivity extends BaseActivity {
+
+    private static final String TAG = "RobotTrackingActivity";
+    private ObjectTrackingView objectTrackingView;
+    private ImageView imageView;
+    private Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_object_tracking);
+
+        imageView = (ImageView) findViewById(R.id.image_view);
+
+        objectTrackingView = (ObjectTrackingView) findViewById(R.id.tracking_view);
+
+        objectTrackingView.setOnOpenCVLoadListener(new OnOpenCVLoadListener() {
+            @Override
+            public void onOpenCVLoadSuccess() {
+                Toast.makeText(getApplicationContext(), "OpenCV 加载成功", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onOpenCVLoadFail() {
+                Toast.makeText(getApplicationContext(), "OpenCV 加载失败", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNotInstallOpenCVManager() {
+                showInstallDialog();
+            }
+        });
+        // 显示反投影图 调试用
+        objectTrackingView.setOnCalcBackProjectListener(new OnCalcBackProjectListener() {
+            @Override
+            public void onCalcBackProject(final Mat backProject) {
+                Log.i(TAG, "onCalcBackProject: " + backProject);
+                ObjectTrackingActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (null == bitmap) {
+                            bitmap = Bitmap.createBitmap(backProject.width(), backProject.height(), Bitmap.Config.ARGB_8888);
+                        }
+                        Utils.matToBitmap(backProject, bitmap);
+                        imageView.setImageBitmap(bitmap);
+                    }
+                });
+            }
+        });
+        // 目标检测回调
+        objectTrackingView.setOnObjectTrackingListener(new OnObjectTrackingListener() {
+            @Override
+            public void onObjectLocation(Point center) {
+                Log.i(TAG, "onObjectLocation: 目标位置 [" + center.x + ", " + center.y + "]");
+            }
+
+            @Override
+            public void onObjectLost() {
+                ObjectTrackingActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "目标丢失", Toast.LENGTH_SHORT).show();
+                        imageView.setImageBitmap(null);
+                    }
+                });
+            }
+        });
+
+        objectTrackingView.loadOpenCV(getApplicationContext());
+    }
+
+    /**
+     * 切换摄像头
+     *
+     * @param view view
+     */
+    public void swapCamera(View view) {
+        objectTrackingView.swapCamera();
+    }
+
+}
